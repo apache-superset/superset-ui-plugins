9a59555b455f52ce1433c4cb6526d11c9033ce51
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-03-15 15:11:27 -0700

    refactor: remove unused function

--- packages/superset-ui-preset-chart-xy/src/encodeable/types/Data.ts
+++ packages/superset-ui-preset-chart-xy/src/encodeable/types/Data.ts
@@ -6,7 +6,3 @@ export type Dataset<T extends string = string> = {
   keys: T[];
   values: Partial<PlainObject<T>>[];
 };
-
-export function verifyDataset<T extends string>(input: Dataset<T>): Dataset<T> {
-  return input;
-}
