e6cc919a6c9ca374b94500e1e167647b93739989
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-04-11 15:53:44 -0700

    build: add script to concat css into style.css

--- buildAssets.js
+++ buildAssets.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies, no-console */
 const fg = require('fast-glob');
 const fs = require('fs-extra');
+const concatenate = require('concatenate');
 
 const packages = fg.sync(['packages/*'], {
   onlyDirectories: true,
 });
 
 packages.forEach(pkg => {
+  // Copy images, geojsons, and raw css
   const assets = fg.sync([`${pkg}/src/**/*.{png,gif,jpg,css,geojson}`]);
   assets.forEach(filePath => {
     const newPaths = ['lib', 'esm'].map(dir => filePath.replace(`${pkg}/src`, `${pkg}/${dir}`));
@@ -20,4 +22,11 @@ packages.forEach(pkg => {
       });
     });
   });
+
+  const cssFiles = fg.sync([`${pkg}/src/**/*.css`]);
+  if (cssFiles.length > 0) {
+    // Concatenate all css into one file
+    concatenate.sync(cssFiles, `${pkg}/esm/style.css`);
+    fs.copyFile(`${pkg}/esm/style.css`, `${pkg}/lib/style.css`);
+  }
 });
