7c5ae58503fa186006884783ee0aa3e89a8777e2
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-08-01 16:00:52 -0700

    fix: disable another

--- packages/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-country-map/index.js
+++ packages/superset-ui-plugins-demo/storybook/stories/legacy-plugin-chart-country-map/index.js
@@ -1,8 +1,8 @@
-import CountryMapChartPlugin from '../../../../superset-ui-legacy-plugin-chart-country-map';
-import Stories from './Stories';
+// import CountryMapChartPlugin from '../../../../superset-ui-legacy-plugin-chart-country-map';
+// import Stories from './Stories';
 
-new CountryMapChartPlugin().configure({ key: 'country-map' }).register();
+// new CountryMapChartPlugin().configure({ key: 'country-map' }).register();
 
-export default {
-  examples: [...Stories],
-};
+// export default {
+//   examples: [...Stories],
+// };
