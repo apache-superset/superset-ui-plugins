d70aeb29db397c963dfea50d45fc2b42bc955d13
Conglei Shi <conglei.shi@airbnb.com>
2019-09-11 16:59:01 -0700

    feat(datatable): render html correctly
    n

--- packages/superset-ui-plugin-chart-table/src/Table.tsx
+++ packages/superset-ui-plugin-chart-table/src/Table.tsx
@@ -58,6 +58,17 @@ function getCellHash(cell: Cell) {
   return `${cell.key}#${cell.value}`;
 }
 
+function getText(value: string | number) {
+  if (typeof value === 'string') {
+    const span = document.createElement('span');
+    span.innerHTML = value;
+
+    return String(span.textContent || span.innerText);
+  }
+
+  return String(value);
+}
+
 class TableVis extends React.PureComponent<InternalTableProps, TableState> {
   static defaultProps = defaultProps;
 
@@ -187,7 +198,7 @@ class TableVis extends React.PureComponent<InternalTableProps, TableState> {
     const keys = dataToRender && dataToRender.length > 0 ? Object.keys(dataToRender[0].data) : [];
     let calculatedWidth = 0;
     keys.forEach(key => {
-      const maxLength = Math.max(...data.map(d => String(d.data[key]).length), key.length);
+      const maxLength = Math.max(...data.map(d => getText(d.data[key]).length), key.length);
       const stringWidth = maxLength * CHAR_WIDTH + CELL_PADDING;
       columnMetadata[key] = {
         maxWidth: MAX_COLUMN_WIDTH,
@@ -195,6 +206,21 @@ class TableVis extends React.PureComponent<InternalTableProps, TableState> {
         ...columnMetadata[key],
       };
       calculatedWidth += Math.min(stringWidth, MAX_COLUMN_WIDTH);
+
+      if (!renderers[key]) {
+        renderers[key] = getRenderer({
+          alignPositiveNegative,
+          colorPositiveNegative,
+          column: {
+            key,
+            label: key,
+            type: 'string',
+          },
+          enableFilter: tableFilter,
+          handleCellSelected: this.handleCellSelected,
+          isSelected: this.isSelected,
+        });
+      }
     });
 
     const tableHeight = includeSearch ? height - SEARCH_BAR_HEIGHT : height;
