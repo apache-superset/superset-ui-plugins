ecd59d5c167e0a99482c2add333268c5a7ec982d
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-11-05 01:51:58 -0800

    fix: memoize

--- packages/superset-ui-plugin-chart-table/src/components/HTMLRenderer.tsx
+++ packages/superset-ui-plugin-chart-table/src/components/HTMLRenderer.tsx
@@ -2,10 +2,10 @@ import React, { useMemo } from 'react';
 import dompurify from 'dompurify';
 
 export default function HTMLRenderer({ value }: { value: string }) {
-  const memoizedHtml = useMemo(() => dompurify.sanitize(value), [value]);
+  const memoizedHtml = useMemo(() => ({ __html: dompurify.sanitize(value) }), [value]);
 
   return (
     // eslint-disable-next-line react/no-danger
-    <div dangerouslySetInnerHTML={{ __html: memoizedHtml }} />
+    <div dangerouslySetInnerHTML={memoizedHtml} />
   );
 }
