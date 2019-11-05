23f9ea56da8354abda4e3e0e659c4dfc51859ee7
Krist Wongsuphasawat <krist.wongz@gmail.com>
2019-11-05 09:03:20 -0800

    fix: bug

--- packages/superset-ui-plugin-chart-table/src/components/HTMLRenderer.tsx
+++ packages/superset-ui-plugin-chart-table/src/components/HTMLRenderer.tsx
@@ -2,10 +2,6 @@ import React, { useMemo } from 'react';
 import dompurify from 'dompurify';
 
 export default function HTMLRenderer({ value }: { value: string }) {
-  if (!value.includes('<') || value.includes('>')) {
-    return <div>{value}</div>;
-  }
-
   const html = useMemo(() => ({ __html: dompurify.sanitize(value) }), [value]);
 
   return (
