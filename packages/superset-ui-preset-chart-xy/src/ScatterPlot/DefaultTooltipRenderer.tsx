6ca6e0bf66a6bae2f9bc88ca6c7d79862c03f346
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-02 16:00:22 -0700

    feat: migrate scatter plot
    (cherry picked from commit 0d607a60cdab8037ece7f29a74bef1c0a5432c5d)

--- packages/superset-ui-preset-chart-xy/src/ScatterPlot/DefaultTooltipRenderer.tsx
+++ packages/superset-ui-preset-chart-xy/src/ScatterPlot/DefaultTooltipRenderer.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TooltipFrame, TooltipTable } from '@superset-ui/chart-composition';
-import { isFieldDef } from '../encodeable/types/ChannelDef';
+import { isFieldDef } from 'encodable/lib/typeGuards/ChannelDef';
 import { TooltipProps } from './ScatterPlot';
 
 export default function DefaultTooltipRenderer({ datum, encoder }: TooltipProps) {
@@ -8,43 +8,43 @@ export default function DefaultTooltipRenderer({ datum, encoder }: TooltipProps)
   const { x, y, size, fill, stroke } = channels;
 
   const tooltipRows = [
-    { key: 'x', keyColumn: x.getTitle(), valueColumn: x.format(datum.data) },
-    { key: 'y', keyColumn: y.getTitle(), valueColumn: y.format(datum.data) },
+    { key: 'x', keyColumn: x.getTitle(), valueColumn: x.formatDatum(datum.data) },
+    { key: 'y', keyColumn: y.getTitle(), valueColumn: y.formatDatum(datum.data) },
   ];
 
   if (isFieldDef(fill.definition)) {
     tooltipRows.push({
       key: 'fill',
       keyColumn: fill.getTitle(),
-      valueColumn: fill.format(datum.data),
+      valueColumn: fill.formatDatum(datum.data),
     });
   }
   if (isFieldDef(stroke.definition)) {
     tooltipRows.push({
       key: 'stroke',
       keyColumn: stroke.getTitle(),
-      valueColumn: stroke.format(datum.data),
+      valueColumn: stroke.formatDatum(datum.data),
     });
   }
   if (isFieldDef(size.definition)) {
     tooltipRows.push({
       key: 'size',
       keyColumn: size.getTitle(),
-      valueColumn: size.format(datum.data),
+      valueColumn: size.formatDatum(datum.data),
     });
   }
   channels.group.forEach(g => {
     tooltipRows.push({
       key: `${g.name}`,
       keyColumn: g.getTitle(),
-      valueColumn: g.format(datum.data),
+      valueColumn: g.formatDatum(datum.data),
     });
   });
   channels.tooltip.forEach(g => {
     tooltipRows.push({
       key: `${g.name}`,
       keyColumn: g.getTitle(),
-      valueColumn: g.format(datum.data),
+      valueColumn: g.formatDatum(datum.data),
     });
   });
 
--- packages/superset-ui-preset-chart-xy/src/ScatterPlot/Encoder.ts
+++ packages/superset-ui-preset-chart-xy/src/ScatterPlot/Encoder.ts
@@ -1,63 +1,39 @@
-import { Value } from 'vega-lite/build/src/channeldef';
-import { ChannelTypeToDefMap } from '../encodeable/types/Channel';
-import { ExtractChannelOutput } from '../encodeable/types/ChannelDef';
-import createEncoderClass from '../encodeable/createEncoderClass';
+import { createEncoderFactory, Encoder } from 'encodable';
+import { DeriveEncoding, DeriveChannelOutputs } from 'encodable/lib/types/Encoding';
 
-/**
- * Define channel types
- */
-const channelTypes = {
-  fill: 'Color',
-  group: 'Text',
-  size: 'Numeric',
-  stroke: 'Color',
-  tooltip: 'Text',
-  x: 'X',
-  y: 'Y',
-} as const; // "as const" is mandatory
-
-export type ChannelTypes = typeof channelTypes;
-
-/**
- * TEMPLATE:
- * Helper for defining encoding
- */
-type CreateChannelDef<
-  ChannelName extends keyof ChannelTypes,
-  Output extends Value
-> = ChannelTypeToDefMap<Output>[ChannelTypes[ChannelName]];
-
-/**
- * Encoding definition
- */
-export type Encoding = {
-  fill: CreateChannelDef<'fill', string>;
-  group: CreateChannelDef<'group', string>[];
-  size: CreateChannelDef<'size', number>;
-  stroke: CreateChannelDef<'stroke', string>;
-  tooltip: CreateChannelDef<'tooltip', string>[];
-  x: CreateChannelDef<'x', number>;
-  y: CreateChannelDef<'y', number>;
+export type ScatterPlotEncodingConfig = {
+  x: ['X', number];
+  y: ['Y', number];
+  fill: ['Color', string];
+  group: ['Category', string, 'multiple'];
+  size: ['Numeric', number];
+  stroke: ['Color', string];
+  tooltip: ['Text', string, 'multiple'];
 };
 
-/**
- * TEMPLATE:
- * Can use this to get returned type of a Channel
- * example usage: ChannelOutput<'x'>
- */
-export type ChannelOutput<ChannelName extends keyof Encoding> = ExtractChannelOutput<
-  Encoding[ChannelName]
->;
-
-export default class Encoder extends createEncoderClass<ChannelTypes, Encoding>({
-  channelTypes,
+export const scatterPlotEncoderFactory = createEncoderFactory<ScatterPlotEncodingConfig>({
+  channelTypes: {
+    x: 'X',
+    y: 'Y',
+    fill: 'Color',
+    group: 'Category',
+    size: 'Numeric',
+    stroke: 'Color',
+    tooltip: 'Text',
+  },
   defaultEncoding: {
+    x: { field: 'x', type: 'quantitative' },
+    y: { field: 'y', type: 'quantitative' },
     fill: { value: '#222' },
     group: [],
     size: { value: 5 },
     stroke: { value: 'none' },
     tooltip: [],
-    x: { field: 'x', type: 'quantitative' },
-    y: { field: 'y', type: 'quantitative' },
   },
-}) {}
+});
+
+export type ScatterPlotEncoding = DeriveEncoding<ScatterPlotEncodingConfig>;
+
+export type ScatterPlotEncoder = Encoder<ScatterPlotEncodingConfig>;
+
+export type ScatterPlotChannelOutputs = DeriveChannelOutputs<ScatterPlotEncodingConfig>;
