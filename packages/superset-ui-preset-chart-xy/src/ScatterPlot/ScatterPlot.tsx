95fee508340b67a85ce0c9df4e992ef84450ae46
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-02 16:54:14 -0700

    feat: migrate scatterplot to use encodable (#421)
    * feat: migrate scatter plot
    
    (cherry picked from commit 0d607a60cdab8037ece7f29a74bef1c0a5432c5d)
    
    * fix: legend

--- packages/superset-ui-preset-chart-xy/src/ScatterPlot/ScatterPlot.tsx
+++ packages/superset-ui-preset-chart-xy/src/ScatterPlot/ScatterPlot.tsx
@@ -3,33 +3,38 @@ import { XYChart, PointSeries } from '@data-ui/xy-chart';
 import { chartTheme, ChartTheme } from '@data-ui/theme';
 import { Margin, Dimension } from '@superset-ui/dimension';
 import { WithLegend } from '@superset-ui/chart-composition';
-import Encoder, { Encoding, ChannelOutput } from './Encoder';
-import { Dataset, PlainObject } from '../encodeable/types/Data';
-import { PartialSpec } from '../encodeable/types/Specification';
+import { isFieldDef } from 'encodable/lib/typeGuards/ChannelDef';
+import { Dataset, PlainObject } from 'encodable/lib/types/Data';
+import {
+  scatterPlotEncoderFactory,
+  ScatterPlotEncoder,
+  ScatterPlotChannelOutputs,
+  ScatterPlotEncodingConfig,
+  ScatterPlotEncoding,
+} from './Encoder';
 import createMarginSelector, { DEFAULT_MARGIN } from '../utils2/createMarginSelector';
 import DefaultTooltipRenderer from './DefaultTooltipRenderer';
 import convertScaleToDataUIScale from '../utils/convertScaleToDataUIScaleShape';
-import { isScaleFieldDef } from '../encodeable/types/ChannelDef';
-import createXYChartLayoutWithTheme from '../utils/createXYChartLayoutWithTheme';
-import createEncoderSelector from '../encodeable/createEncoderSelector';
-import createRenderLegend from '../components/legend/createRenderLegend';
-import { LegendHooks } from '../components/legend/types';
+import createXYChartLayoutWithTheme from '../utils2/createXYChartLayoutWithTheme';
+import createRenderLegend from '../components/legend2/createRenderLegend';
+import { LegendHooks } from '../components/legend2/types';
 
 export interface TooltipProps {
   datum: EncodedPoint;
-  encoder: Encoder;
+  encoder: ScatterPlotEncoder;
 }
 
 const defaultProps = {
   className: '',
   margin: DEFAULT_MARGIN,
+  encoding: {},
   theme: chartTheme,
   TooltipRenderer: DefaultTooltipRenderer,
 } as const;
 
 export type HookProps = {
   TooltipRenderer?: React.ComponentType<TooltipProps>;
-} & LegendHooks<Encoder>;
+} & LegendHooks<ScatterPlotEncodingConfig>;
 
 type Props = {
   className?: string;
@@ -37,24 +42,17 @@ type Props = {
   height: string | number;
   margin?: Margin;
   data: Dataset;
+  encoding?: Partial<ScatterPlotEncoding>;
   theme?: ChartTheme;
-} & PartialSpec<Encoding> &
-  HookProps &
+} & HookProps &
   Readonly<typeof defaultProps>;
 
-export interface EncodedPoint {
-  x: ChannelOutput<'x'>;
-  y: ChannelOutput<'y'>;
-  size: ChannelOutput<'size'>;
-  fill: ChannelOutput<'fill'>;
-  stroke: ChannelOutput<'stroke'>;
-  group: ChannelOutput<'group'>[];
-  tooltip: ChannelOutput<'tooltip'>[];
+export type EncodedPoint = ScatterPlotChannelOutputs & {
   data: PlainObject;
-}
+};
 
 export default class ScatterPlot extends PureComponent<Props> {
-  private createEncoder = createEncoderSelector(Encoder);
+  private createEncoder = scatterPlotEncoderFactory.createSelector();
 
   private createMargin = createMarginSelector();
 
@@ -68,34 +66,18 @@ export default class ScatterPlot extends PureComponent<Props> {
 
   renderChart(dim: Dimension) {
     const { width, height } = dim;
-    const { data, margin, theme, TooltipRenderer } = this.props;
-    const encoder = this.createEncoder(this.props);
+    const { data, margin, theme, TooltipRenderer, encoding } = this.props;
+    const encoder = this.createEncoder(encoding);
     const { channels } = encoder;
 
-    if (typeof channels.x.scale !== 'undefined') {
-      const xDomain = channels.x.getDomain(data);
-      channels.x.scale.setDomain(xDomain);
-    }
-    if (typeof channels.y.scale !== 'undefined') {
-      const yDomain = channels.y.getDomain(data);
-      channels.y.scale.setDomain(yDomain);
-    }
-    if (
-      isScaleFieldDef(channels.size.definition) &&
-      channels.size.definition.type === 'quantitative'
-    ) {
-      const domain = channels.size.getDomain(data) as number[];
-      const [min, max] = domain;
-      const adjustedDomain = [Math.min(min || 0, 0), Math.max(max || 1, 1)];
-      channels.size.scale!.setDomain(adjustedDomain);
-    }
+    encoder.setDomainFromDataset(data);
 
     const encodedData = data.map(d => ({
-      x: channels.x.get(d),
-      y: channels.y.get(d),
-      size: channels.size.encode(d),
-      fill: channels.fill.encode(d),
-      stroke: channels.stroke.encode(d),
+      x: channels.x.encodeDatum(d),
+      y: channels.y.encodeDatum(d),
+      size: channels.size.encodeDatum(d),
+      fill: channels.fill.encodeDatum(d),
+      stroke: channels.stroke.encodeDatum(d),
       data: d,
     }));
 
@@ -119,13 +101,13 @@ export default class ScatterPlot extends PureComponent<Props> {
           <TooltipRenderer datum={datum} encoder={encoder} />
         )}
         theme={theme}
-        xScale={convertScaleToDataUIScale(channels.x.scale!.config)}
-        yScale={convertScaleToDataUIScale(channels.y.scale!.config)}
+        xScale={convertScaleToDataUIScale(channels.x.definition.scale as any)}
+        yScale={convertScaleToDataUIScale(channels.y.definition.scale as any)}
       >
         {layout.renderXAxis()}
         {layout.renderYAxis()}
         <PointSeries
-          key={channels.x.definition.field}
+          key={isFieldDef(channels.x.definition) ? channels.x.definition.field : ''}
           data={encodedData}
           fill={(d: EncodedPoint) => d.fill}
           fillOpacity={0.5}
@@ -137,9 +119,9 @@ export default class ScatterPlot extends PureComponent<Props> {
   }
 
   render() {
-    const { className, data, width, height } = this.props;
+    const { className, data, width, height, encoding } = this.props;
 
-    const encoder = this.createEncoder(this.props);
+    const encoder = this.createEncoder(encoding);
 
     return (
       <WithLegend
