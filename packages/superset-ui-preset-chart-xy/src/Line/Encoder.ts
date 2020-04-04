bf71675288c049b2756997e16c7c5cd6349f4111
Krist Wongsuphasawat <krist.wongz@gmail.com>
2020-04-03 17:12:27 -0700

    feat: migrate line chart to use encodable (#427)
    * feat: migrate line to use encodable
    
    * fix: storybook ts
    
    * fix: remove comment

--- packages/superset-ui-preset-chart-xy/src/Line/Encoder.ts
+++ packages/superset-ui-preset-chart-xy/src/Line/Encoder.ts
@@ -1,63 +1,36 @@
-import { Value } from 'vega-lite/build/src/channeldef';
-import { ChannelTypeToDefMap } from '../encodeable/types/Channel';
-import { ExtractChannelOutput } from '../encodeable/types/ChannelDef';
-import createEncoderClass from '../encodeable/createEncoderClass';
+import { createEncoderFactory, Encoder } from 'encodable';
+import { DeriveEncoding, DeriveChannelOutputs } from 'encodable/lib/types/Encoding';
 
-/**
- * Define channel names and their types
- */
-const channelTypes = {
-  fill: 'Category',
-  stroke: 'Color',
-  strokeDasharray: 'Category',
-  strokeWidth: 'Numeric',
-  x: 'X',
-  y: 'Y',
-} as const;
-
-export type ChannelTypes = typeof channelTypes;
-
-/**
- * TEMPLATE:
- * Helper for defining encoding
- */
-type CreateChannelDef<
-  ChannelName extends keyof ChannelTypes,
-  Output extends Value
-> = ChannelTypeToDefMap<Output>[ChannelTypes[ChannelName]];
-
-/**
- * Encoding definition
- */
-export type Encoding = {
-  fill: CreateChannelDef<'fill', boolean>;
-  stroke: CreateChannelDef<'stroke', string>;
-  strokeDasharray: CreateChannelDef<'strokeDasharray', string>;
-  strokeWidth: CreateChannelDef<'strokeWidth', number>;
-  x: CreateChannelDef<'x', number>;
-  y: CreateChannelDef<'y', number>;
+export type LineEncodingConfig = {
+  x: ['X', number];
+  y: ['Y', number];
+  fill: ['Category', boolean];
+  stroke: ['Color', string];
+  strokeDasharray: ['Category', string];
+  strokeWidth: ['Numeric', number];
 };
 
-/**
- * TEMPLATE:
- * Can use this to get returned type of a Channel
- * example usage: ChannelOutput<'x'>
- */
-export type ChannelOutput<ChannelName extends keyof Encoding> = ExtractChannelOutput<
-  Encoding[ChannelName]
->;
-
-export default class Encoder extends createEncoderClass<ChannelTypes, Encoding>({
-  allChannelOptions: {
-    fill: { legend: false },
+export const lineEncoderFactory = createEncoderFactory<LineEncodingConfig>({
+  channelTypes: {
+    x: 'X',
+    y: 'Y',
+    fill: 'Category',
+    stroke: 'Color',
+    strokeDasharray: 'Category',
+    strokeWidth: 'Numeric',
   },
-  channelTypes,
   defaultEncoding: {
-    fill: { value: false },
+    x: { field: 'x', type: 'quantitative' },
+    y: { field: 'y', type: 'quantitative' },
+    fill: { value: false, legend: false },
     stroke: { value: '#222' },
     strokeDasharray: { value: '' },
     strokeWidth: { value: 1 },
-    x: { field: 'x', type: 'quantitative' },
-    y: { field: 'y', type: 'quantitative' },
   },
-}) {}
+});
+
+export type LineEncoding = DeriveEncoding<LineEncodingConfig>;
+
+export type LineEncoder = Encoder<LineEncodingConfig>;
+
+export type LineChannelOutputs = DeriveChannelOutputs<LineEncodingConfig>;
